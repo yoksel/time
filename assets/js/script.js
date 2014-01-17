2b21a0d620fe13f9e5cbd46261ed7693f71dd52b
yoksel <yoksel@yandex.ru>
2014-01-17 18:44:31 +0400

    Plaing with colours

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,21 @@
+var prev = 0;
+var digits_map = [
+	[".item--1", ".item--2", ".item--3", ".item--5", ".item--6", ".item--7"],
+	[".item--3", ".item--6"],
+	[".item--1", ".item--3", ".item--4", ".item--5", ".item--7"],
+	[".item--1", ".item--3", ".item--4", ".item--6", ".item--7"],
+	[".item--2", ".item--3", ".item--4", ".item--6"],
+	[".item--1", ".item--2", ".item--4", ".item--6", ".item--7"],
+	[".item--1", ".item--2", ".item--4", ".item--5", ".item--6", ".item--7"],
+	[".item--1", ".item--3", ".item--6"],
+	["item"],
+	[".item--1", ".item--2", ".item--3", ".item--4", ".item--6", ".item--7"]
+	];
 
 setTime();
-var interval = setInterval(setTime,1000);
+var interval = setInterval(setTime,1000);	
+
+var class_color = "js-color--crimson";
 
 function setTime() {
 
@@ -14,15 +29,39 @@ function setTime() {
 	// Change Time
 	var digits = document.querySelectorAll('.digit');
 	for ( var i = 0; i < digits.length; i++ ) {
-		var digit = digits[i];
-		var classes = digit.classList;
 		
-		for ( var k = 0; k < classes.length; k++ ){
-			if ( classes[k].indexOf('js') >= 0 ){
-				digit.classList.remove(classes[k]);
-			}
+		var digit = digits[i];
+		var digit_value = +date_string.charAt(i);
+
+		if ( digit_value != prev ){
+
+			var digit_elems_classes = digits_map[i];
+
+			// console.log ("-- Digit: " + digit_value);
+			// console.log(digit_elems_classes);
+
+			for ( var l = 0; l < digit_elems_classes.length; l++ ){
+				var item = digit.querySelector(digit_elems_classes[l]);
+				// console.log ( item );
+				remove_class_from_list (item, "js-color");
+				item.classList.add( class_color );
+				}
+
+			// remove_class_from_list (digit, "js-digit");
+		
+			// digit.classList.add("js-digit--" + date_string.charAt(i));
+			// prev = digit_value;
 		}
-		digit.classList.add("js-digit--" + date_string.charAt(i));
 	}
 
+}
+
+function remove_class_from_list (elem, class_prefix) {
+	var classes = elem.classList;
+			
+	for ( var k = 0; k < classes.length; k++ ){
+		if ( classes[k].indexOf(class_prefix) >= 0 ){
+			elem.classList.remove(classes[k]);
+		}
+	}
 }
